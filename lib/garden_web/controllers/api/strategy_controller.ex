c36eb92053497e449643fb15e85df8f93375cdd7
treejamie <me@treejamie.com>
2025-05-20 22:11:42 +0100

    added untracked strategy files

---
+++ lib/garden_web/controllers/api/strategy_controller.ex
@@ -0,0 +1,33 @@
+defmodule GardenWeb.API.StrategyController do
+  use GardenWeb, :controller
+  alias Garden.Plans
+  # alias Garden.Plan
+  alias Garden.Repo
+
+  def create(conn, params) do
+    case Plans.create_strategy_and_plans_atomically(params) do
+      {:ok, strategy} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", ~p"/v1/strategies/#{strategy.id}")
+        |> render(:show, strategy: Repo.preload(strategy, [:layout, :plans]))
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:error, changeset: changeset)
+      end
+
+  end
+
+  def show(conn, %{"id" => id}) do
+    case Plans.get_strategy(id) do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "Strategy not found"})
+      strategy ->
+        conn
+        |> render(:show, strategy: Repo.preload(strategy, [:layout, :plans]))
+    end
+  end
+end
---
+++ lib/garden_web/controllers/api/strategy_json.ex
@@ -0,0 +1,21 @@
+defmodule GardenWeb.API.StrategyJSON do
+  def error(%{changeset: changeset}) do
+    errors =
+      Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+        Enum.reduce(opts, msg, fn {key, value}, acc ->
+          String.replace(acc, "%{#{key}}", to_string(value))
+        end)
+      end)
+
+    %{errors: errors}
+  end
+
+  def show(%{strategy: strategy}) do
+    %{
+      id: strategy.id,
+      name: strategy.name,
+      description: strategy.description,
+      score: strategy.score
+    }
+  end
+end
