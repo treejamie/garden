b0ab08f37586c2e8c9cfc23fdcb1f1609bacc5cc
treejamie <me@treejamie.com>
2025-05-23 08:24:32 +0100

    getting neater

--- lib/garden/api.ex
+++ lib/garden/api.ex
@@ -1,13 +1,36 @@
 defmodule Garden.API do
-
-
+  alias Garden.Plans
 
   def build_layout_attrs(attrs) do
     Map.drop(attrs, ["beds"])
   end
 
   def build_beds_attrs(attrs) do
-    beds_attrs = Map.get(attrs, "beds", [])
+    Map.get(attrs, "beds", [])
   end
 
+  @doc """
+  Soil id may be a name or an id
+  """
+  def get_soil_by_id_or_name(%{"soil_id" => identifier}) do
+    case Plans.get_soil(identifier) do
+      nil -> {:soil_error, :get_soil_by_id_or_name, identifier}
+      soil -> {:ok, soil}
+    end
+  end
+  def get_soil_by_id_or_name(identifier), do: {:soil_error, identifier}
+
+  @doc """
+  Builds bed attrs with a given layout id
+  """
+  def build_bed_attrs(attrs, layout_id) do
+    with attrs <- Map.put(attrs, "layout_id", layout_id),
+         {:ok, soil} <- get_soil_by_id_or_name(attrs),
+         attrs <- Map.put(attrs, "soil_id", soil.id) do
+      {:ok, attrs}
+    else
+      {:soil_error, reason, id }  -> {:soil_error, reason, id }
+      _ -> {:error, attrs}
+    end
+  end
 end
--- test/garden/api_test.exs
+++ test/garden/api_test.exs
@@ -8,29 +8,48 @@ defmodule Garden.APITest do
   alias Garden.Plans
 
   @tag :sword
-  test "build_beds_attrs" do
+  test "build_bed_attrs - happy path" do
     # make the background knowledge - just need soil for beds
-    {:ok, _soil} = Plans.create_soil(%{name: "loam"})
+    {:ok, soil} = Plans.create_soil(%{name: "chalk"})
+
+    # make the attrs
+    attrs = %{"soil_id" => "chalk", "x" => 1, "y" => 0, "l" => 2, "w" => 2}
+    layout_id = 2
+
+    # make the attrs and we get back an expected map with a soil id
+    {:ok, bed_attrs} = API.build_bed_attrs(attrs, layout_id)
+    assert bed_attrs == %{"soil_id" => soil.id, "x" => 1, "y" => 0, "l" => 2, "w" => 2, "layout_id" => 2}
+
+    IO.inspect(bed_attrs)
+  end
+
+  @tag :sword
+  test "build_beds_attrs  - happy path" do
+    # make the background knowledge - just need soil for beds
+    # {:ok, _soil} = Plans.create_soil(%{name: "chalk"})
 
     # make the attrs
     attrs = %{
       "name" => "Kew",
       "beds" => [
-        %{"soil_id" => "chalk", "x" => 1, "y" => 0, "l" => 2, "w" => 2}
+        %{"soil_id" => "chalk", "x" => 1, "y" => 0, "l" => 2, "w" => 2},
+        %{"soil_id" => "chalk", "x" => 3, "y" => 3, "l" => 2, "w" => 2}
       ]
     }
 
-    # make the attrs for the layout and we get back an expected map
+    # make the attrs for the beds and we get back an expected map
     beds_attrs = API.build_beds_attrs(attrs)
 
-    assert [%{"l" => 2, "soil_id" => "chalk", "w" => 2, "x" => 1, "y" => 0}] =
-             beds_attrs
+    assert [
+             %{"soil_id" => "chalk", "x" => 1, "y" => 0, "l" => 2, "w" => 2},
+             %{"soil_id" => "chalk", "x" => 3, "y" => 3, "l" => 2, "w" => 2}
+           ] = beds_attrs
   end
 
   @tag :sword
-  test "build_layout_attrs" do
+  test "build_layout_attrs - happy path" do
     # make the background knowledge - just need soil for beds
-    {:ok, _soil} = Plans.create_soil(%{name: "loam"})
+    # {:ok, _soil} = Plans.create_soil(%{name: "loam"})
 
     # make the attrs
     attrs = %{
